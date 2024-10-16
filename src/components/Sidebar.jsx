7182a3bdb6c886cf7fc7cb11c4b03e617e4216e1
gabriel.pereira <gasouzape@gmail.com>
2024-10-15 21:55:18 -0300

    finalizing profile

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import styles from './Sidebar.module.css';
 
+import { PencilLine } from '@phosphor-icons/react'
+
 export function Sidebar(){
     return (
         <aside class={styles.sidebar}>
@@ -9,12 +11,17 @@ export function Sidebar(){
             />
             
             <div className={styles.profile}>
+                <img 
+                    className={styles.avatar}
+                    src="https://github.com/gasouzapd.png" 
+                    />
                 <strong>Gabriel Pereira</strong>
                 <span>Web Developer</span>
             </div>
             
             <footer>
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
             </footer>
