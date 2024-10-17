e2819eb4e347d8bc76a92b239aaf1b996db55a53
gabriel.pereira <gasouzape@gmail.com>
2024-10-16 23:37:31 -0300

    evolving form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Header } from './components/Header';
-import { Post } from './Post';
+import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 
 import './global.css';
@@ -13,15 +13,8 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Gabriel Pereira" 
-            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat facere porro veniam magni minus perferendis omnis harum libero aspernatur. Neque repellat eos vel et quod consectetur, dolore ipsa numquam accusantium."
-          />
-
-          <Post 
-            author="Gabriel Pereira1 " 
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae facere quam iste tenetur nemo consequatur, quasi velit blanditiis, laborum ipsam cum adipisci! Itaque natus, impedit aperiam et voluptates quos iste?"
-          />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
