f331e2a4c9aa8795c69e6433e8fff03134f9180c
gabriel.pereira <gasouzape@gmail.com>
2024-10-18 19:35:11 -0300

    finalizing comment

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,8 @@ function App() {
         <main>
           <Post />
           <Post />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,7 @@ import styles from './Comment.module.css';
 
 export function Comment(){
     return (
-        <div>
+        <div className={styles.comment}>
             <img src="https://github.com/gasouzape.png"/>
 
             <div className={styles.commentBox}>
@@ -15,7 +15,7 @@ export function Comment(){
                         </div>
 
                         <button tittle="Deletar comentário">
-                            <Trash size={20}/>
+                            <Trash size={24}/>
                         </button>
                     </header>
                     
