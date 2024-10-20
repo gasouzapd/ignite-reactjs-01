7917eb92ebe26a385bfdcbd255cbec6c827a9c05
gabriel.pereira <gasouzape@gmail.com>
2024-10-19 21:33:11 -0300

    composing avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,18 @@
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 import { PencilLine } from '@phosphor-icons/react'
 
 export function Sidebar(){
     return (
         <aside className={styles.sidebar}>
-            <img 
+            <img  
                 className={styles.cover}
                 src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             />
             
             <div className={styles.profile}>
-                <img 
+                <Avatar  
                     className={styles.avatar}
                     src="https://github.com/gasouzape.png" 
                     />
